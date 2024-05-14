982e9c8a5fe463b3a570c29e04b314a45396ff9c
Patel Muhammad <96219910+Patel-Muhammad@users.noreply.github.com>
2024-05-14 09:49:16 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -6,20 +7,22 @@ driver = webdriver.Chrome()
 
 driver.get("https://deployfor.me")
 
-repo_input = driver.find_element_by_xpath("//input[@placeholder='https://github.com/profile/react-project']")
+time.sleep(15)
+
+repo_input = driver.find_element(By.CLASS_NAME, "github-url")
 repo_input.send_keys("https://github.com/Patel-Muhammad/react-boilerplate-test")
 
-domain_input = driver.find_element_by_xpath("//input[@placeholder='mycoolproject']")
+domain_input = driver.find_element(By.CLASS_NAME, "domain-name")
 domain_input.send_keys("testdomain")
 
-deploy_button = driver.find_element_by_xpath("//button[contains(@class, 'bg-gray-100')]")
+deploy_button = driver.find_element(By.CLASS_NAME, "deploy-button")
 deploy_button.click()
 
-time.sleep(30)
+time.sleep(60)
 
 driver.get("http://testdomain.deployfor.me")
 
-if "Test Project" in driver.title: # Adjust the title as per your project
+if "Test Project" in driver.title:
     print("Deployment successful")
 else:
     print("Deployment failed")

90e2a7bd03482574565d77e828ed597023bc8a13
Patel Muhammad <96219910+Patel-Muhammad@users.noreply.github.com>
2024-05-14 11:52:34 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -3,7 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome(executable_path="./chromedriver")
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "./chromedriver" 
+driver = webdriver.Chrome(options=options)
+
 driver.get("https://deployfor.me")
 
 time.sleep(15)
